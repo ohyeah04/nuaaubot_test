6642132f2171c5a15156bc51664993c8655f67a3
ohyeah <davletiyarov@gmail.com>
2020-08-06 21:23:29 +0600

    test new deploy

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot('1252501384:AAGodt4oSf6X6EzvwyZr7ux265dKHve3NOo')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Button1', 'Button2 hello2')
+
+@bot.message_handler(commands=['start'])
+
+def start_message(message):
+    bot.send_message(message.chat.id, 'hi there now i will show u menu', reply_markup=keyboard1)
+
+bot.polling()
